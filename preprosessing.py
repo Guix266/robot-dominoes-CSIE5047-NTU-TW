1db4823d60b2d13125184db9f2763db94b29e9af
ananasikgol <ananasik.gol@gmail.com>
2019-12-08 22:26:39 +0800

    Update preprosessing.py
    fixed the sixes

--- preprosessing.py
+++ preprosessing.py
@@ -21,34 +21,49 @@ for filename in os.listdir(folder):
     if img is not None:
         images.append(img)
 bwImg = []
+edgeImg = []
 
-fig, ax = plt.subplots(5, 6, sharex=True, sharey=True, dpi=300)
+figEdge, axEdge = plt.subplots(5, 6, sharex=True, sharey=True, dpi=300)
+figBw, axBw = plt.subplots(5, 6, sharex=True, sharey=True, dpi=300)
 figIm, axIm = plt.subplots(1, 2, dpi = 300)
 
 # preprosessing
-contrast = 1.3
+contrast = 1.0
 for i in range(len(images)):
     img = images[i]
     img = cv.resize(img, (200, 266))
     # change constrast
     img = np.minimum(255*np.ones((266, 200, 3)), img*contrast)
+    # make colorfull pixels darker
+    pixelStd = np.dstack((2*np.std(img, axis = 2), )*3)
+    img = np.maximum(255*np.zeros((266, 200, 3)), img - pixelStd)
     img = np.asarray(img, dtype="uint8" )
+    images[i] = img
     # turn image to grayscale
     bw = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    # (thres, bw) = cv.threshold(bw,70,255,cv.THRESH_TOZERO)
+    # set background to black
+    (thres, bw) = cv.threshold(bw, 70, 255, cv.THRESH_TOZERO)
+    # canny edge 
+    edge = cv.Canny(bw, 200, 200)
     # bw = cv.adaptiveThreshold(bw, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
     #                            cv.THRESH_BINARY,101,2)
     (thres, bw) = cv.threshold(bw,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+    # (im2, contours, hierarchy) = cv.findContours(bw, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    # cv.drawContours(img, contours, -1, (0,200,0), 1)
     bwImg.append(bw)
-    ax[i//6, i%6].imshow(bw, cmap='binary')
+    edgeImg.append(edge)
+    axBw[i//6, i%6].imshow(bw, cmap='binary')
+    axEdge[i//6, i%6].imshow(edge, cmap='binary')
+    
+
 
 # display one image on a large axis for further examination
-displayID = 5
+displayID = 11
+# axIm[0].imshow(edgeImg[displayID], cmap='binary')
 axIm[0].imshow(bwImg[displayID], cmap='binary')
 axIm[1].imshow(images[displayID])
 axIm[0].xaxis.set_ticks([])
 axIm[0].yaxis.set_ticks([])
 axIm[1].xaxis.set_ticks([])
 axIm[1].yaxis.set_ticks([])
-
 
