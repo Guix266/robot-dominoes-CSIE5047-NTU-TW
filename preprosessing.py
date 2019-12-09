12a18c9251eecb0bfbc58e339311f0691b2cc179
ananasikgol <ananasik.gol@gmail.com>
2019-12-09 14:22:30 +0800

    Update preprosessing.py
    recognition of single dominoes

--- preprosessing.py
+++ preprosessing.py
@@ -22,10 +22,13 @@ for filename in os.listdir(folder):
         images.append(img)
 bwImg = []
 edgeImg = []
+colorImg = []
+contoursAll = []
+areas = []
 
-figEdge, axEdge = plt.subplots(5, 6, sharex=True, sharey=True, dpi=300)
-figBw, axBw = plt.subplots(5, 6, sharex=True, sharey=True, dpi=300)
-figIm, axIm = plt.subplots(1, 2, dpi = 300)
+# figEdge, axEdge = plt.subplots(5, 6, sharex=True, sharey=True, dpi=300)
+# figBw, axBw = plt.subplots(5, 6, sharex=True, sharey=True, dpi=300)
+figIm, axIm = plt.subplots(1, 3, dpi = 300)
 
 # preprosessing
 contrast = 1.0
@@ -38,7 +41,6 @@ for i in range(len(images)):
     pixelStd = np.dstack((2*np.std(img, axis = 2), )*3)
     img = np.maximum(255*np.zeros((266, 200, 3)), img - pixelStd)
     img = np.asarray(img, dtype="uint8" )
-    images[i] = img
     # turn image to grayscale
     bw = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     # set background to black
@@ -48,22 +50,42 @@ for i in range(len(images)):
     # bw = cv.adaptiveThreshold(bw, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,\
     #                            cv.THRESH_BINARY,101,2)
     (thres, bw) = cv.threshold(bw,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
-    # (im2, contours, hierarchy) = cv.findContours(bw, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    # cv.drawContours(img, contours, -1, (0,200,0), 1)
+    (im2, contours, hierarchy) = cv.findContours(bw, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+    dominoContours = []
+    for cont in contours:
+        if (cv.contourArea(cont) > 400
+            and cv.contourArea(cont) < 1800
+            and cv.isContourConvex):
+            dominoContours.append(cont)
+            areas.append(cv.contourArea(cont))
+            rect = cv.minAreaRect(cont)
+            box = cv.boxPoints(rect)
+            box = np.int0(box)
+            cv.drawContours(img,[box],0,(0,0,255),2)
+            # check for the color?
+    # cv.drawContours(img, dominoContours, -1, (0,200,0), 1)
     bwImg.append(bw)
     edgeImg.append(edge)
-    axBw[i//6, i%6].imshow(bw, cmap='binary')
-    axEdge[i//6, i%6].imshow(edge, cmap='binary')
+    colorImg.append(img)
+    contoursAll.append(dominoContours)
+    # axBw[i//6, i%6].imshow(bw, cmap='binary')
+    # axEdge[i//6, i%6].imshow(edge, cmap='binary')
     
-
 
 # display one image on a large axis for further examination
 displayID = 11
 # axIm[0].imshow(edgeImg[displayID], cmap='binary')
-axIm[0].imshow(bwImg[displayID], cmap='binary')
-axIm[1].imshow(images[displayID])
-axIm[0].xaxis.set_ticks([])
-axIm[0].yaxis.set_ticks([])
-axIm[1].xaxis.set_ticks([])
-axIm[1].yaxis.set_ticks([])
+axIm[0].imshow(colorImg[displayID])
+axIm[0].set_title(r'Starting image')
+axIm[1].imshow(bwImg[displayID], cmap='binary')
+axIm[1].set_title('Bitmap image')
+axIm[2].imshow(edgeImg[displayID], cmap='binary')
+axIm[2].set_title('Canny edges')
+
+fig, ax = plt.subplots(1, 1, dpi = 300)
+n, bins, patches = plt.hist(areas, 500, density=True)
+
+for ax in axIm:
+    ax.xaxis.set_ticks([])
+    ax.yaxis.set_ticks([])
 
