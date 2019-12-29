eb4da34c4d266037a8e39fe3e014d95e5e33df22
Guix266 <guillaume.desermeaux@gmail.com>
2019-12-29 15:44:36 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,8 +14,21 @@ import image_recognition
 import AI_v2
 import arm
 
-### Start the game
+# =============================================================================
+# 1rst round
+# =============================================================================
+
+# The robot go on top of the board and recognise the first domino
+#***
+
+# Recognition of the board
+#*** Camera take a picture
+result = image_recognition.find_dominoes(img, 1)
 
 
+#Place the first domino on the board from the stock
+Board = []
+Board.append(AI_v2.Starting_Domino(stock[0], 290, 0, 90))
+stock = stock[1:]
 
 image_recognition.find_dominoes(img, N)
